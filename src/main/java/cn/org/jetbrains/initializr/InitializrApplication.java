79eea81fa46c8833049494f0f6ce41c8c0ed19bc
MR.Chang <mrchang@jetbrains.org.cn>
2017-10-24 19:27:53 +0800

    commit

---
+++ src/main/java/cn/org/jetbrains/initializr/InitializrApplication.java
@@ -0,0 +1,12 @@
+package cn.org.jetbrains.initializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitializrApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/org/jetbrains/initializr/InitializrApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.org.jetbrains.initializr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InitializrApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
