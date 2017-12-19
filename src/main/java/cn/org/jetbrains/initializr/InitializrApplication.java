15d936c7861a96fbdd5bdfabd575553449905a2d
MR.Chang <mrchang@jetbrains.org.cn>
2017-12-19 14:43:48 +0800

    master

--- src/main/java/cn/org/jetbrains/initializr/InitializrApplication.java
+++ src/main/java/cn/org/jetbrains/initializr/InitializrApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InitializrApplication {
 
     public static void main(String[] args) {
-        //dev 合并到 master test dev
+        //dev 合并到 master test master
         SpringApplication.run(InitializrApplication.class, args);
     }
 }
