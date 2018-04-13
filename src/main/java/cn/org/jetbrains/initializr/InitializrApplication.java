7697327c1a3ad3ac46888ddc5e61f5bf7418fd75
MR.Chang <mrchang@jetbrains.org.cn>
2018-04-13 12:13:58 +0800

    增加eureka  client 配置

--- src/main/java/cn/org/jetbrains/initializr/InitializrApplication.java
+++ src/main/java/cn/org/jetbrains/initializr/InitializrApplication.java
@@ -3,9 +3,11 @@ package cn.org.jetbrains.initializr;
 import io.spring.initializr.metadata.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InitializrApplication {
 
     public static void main(String[] args) {
